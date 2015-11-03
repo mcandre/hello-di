0daaf6dd60d42e9fbc864937f0e84f1f4e4e140a
Andrew Pennebaker <andrew.pennebaker@shoutlet.com>
2015-11-03 11:37:56 -0600

    lint

--- src/main/java/us/yellosoft/hellodi/TabletopServer.java
+++ src/main/java/us/yellosoft/hellodi/TabletopServer.java
@@ -5,8 +5,15 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 /**
    An example bootstrap of a Dependency Injected game engine
  */
-public class TabletopServer {
-  public static void main(String[] args) {
+public final class TabletopServer {
+  /** Utility class */
+  private TabletopServer() {}
+
+  /**
+     CLI entry point
+     @param args CLI flags
+   */
+  public static void main(final String[] args) {
     AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
     context.register(TabletopConfiguration.class);
     context.register(DeepThoughtRNG.class);
